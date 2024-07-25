bf0211b8ce1e69d1c84fe67e72951eaf658b855c
Pedro Cattori <pcattori@gmail.com>
2024-07-25 14:04:34 -0400

    virtual language service + host

--- packages/ts-plugin/src/index.ts
+++ packages/ts-plugin/src/index.ts
@@ -1,61 +1,184 @@
+// Adapted from https://github.com/sveltejs/language-tools/blob/527c2adb2fc6f13674bcc73cf52b63370dc0c8db/packages/typescript-plugin/src/language-service/sveltekit.ts
+
 import type ts from "typescript/lib/tsserverlibrary"
+type TS = typeof ts
 
-function init(modules: { typescript: typeof ts }) {
-  const ts = modules.typescript
+// plugin
+// ----------------------------------------------------------------------------
 
-  function create(info: ts.server.PluginCreateInfo) {
-    // Get a list of things to remove from the completion list from the config object.
-    // If nothing was specified, we'll just remove 'caller'
-    const whatToRemove: string[] = info.config.remove || ["caller"]
+function init(modules: { typescript: TS }) {
+  // const ts = modules.typescript
 
-    // Diagnostic logging
-    info.project.projectService.logger.info(
-      "I'm getting set up now! Check the log for this message.",
-    )
-
-    // Set up decorator object
-    const proxy: ts.LanguageService = Object.create(null)
-    for (let k of Object.keys(info.languageService) as Array<
-      keyof ts.LanguageService
-    >) {
-      const x = info.languageService[k]!
-      // @ts-expect-error - JS runtime trickery which is tricky to type tersely
-      proxy[k] = (...args: Array<{}>) => x.apply(info.languageService, args)
-    }
-
-    // Remove specified entries from completion list
-    proxy.getCompletionsAtPosition = (fileName, position, options) => {
-      // This is just to let you hook into something to
-      // see the debugger working
-      debugger
-
-      const prior = info.languageService.getCompletionsAtPosition(
-        fileName,
-        position,
-        options,
-      )
-      if (!prior) return
-
-      const oldLength = prior.entries.length
-      prior.entries = prior.entries.filter(
-        (e) => whatToRemove.indexOf(e.name) < 0,
-      )
-
-      // Sample logging for diagnostic purposes
-      if (oldLength !== prior.entries.length) {
-        const entriesRemoved = oldLength - prior.entries.length
-        info.project.projectService.logger.info(
-          `Removed ${entriesRemoved} entries from the completion list`,
-        )
-      }
-
-      return prior
-    }
-
-    return proxy
-  }
+  function create(/* info: ts.server.PluginCreateInfo */) {}
 
   return { create }
 }
-
 export = init
+
+// virtual
+// ----------------------------------------------------------------------------
+
+type RouteSnapshot = {
+  file: ts.IScriptSnapshot
+  version: string
+}
+
+type VirtualLanguageServiceHost = {
+  getRouteScriptSnapshotIfUpToDate: (
+    fileName: string,
+  ) => RouteSnapshot | undefined
+  upsertRouteFile: (fileName: string) => void
+}
+
+const FORCE_UPDATE_VERSION = "FORCE_UPDATE_VERSION"
+
+const CACHE = new WeakMap<
+  ts.server.PluginCreateInfo,
+  {
+    languageService: ts.LanguageService
+    languageServiceHost: ts.LanguageServiceHost & VirtualLanguageServiceHost
+  } | null
+>()
+
+function getVirtualLanguageService(info: ts.server.PluginCreateInfo, ts: TS) {
+  const cached = CACHE.get(info)
+  if (cached) return cached
+
+  const host = info.languageServiceHost
+
+  class VirtualLanguageServiceHost implements ts.LanguageServiceHost {
+    private files: Record<string, RouteSnapshot> = {}
+
+    constructor() {}
+
+    // TODO: Q: are these needed? do they just "silence" the virtual host?
+    // log() {}
+    // trace() {}
+    // error() {}
+
+    getCompilationSettings = host.getCompilationSettings
+    getCurrentDirectory = host.getCurrentDirectory
+    getDefaultLibFileName = host.getDefaultLibFileName
+
+    getScriptFileNames(): string[] {
+      const names: Set<string> = new Set(Object.keys(this.files))
+      const files = host.getScriptFileNames()
+      for (const file of files) {
+        names.add(file)
+      }
+      return [...names]
+    }
+
+    getScriptVersion(fileName: string) {
+      const file = this.files[fileName]
+      if (!file) return host.getScriptVersion(fileName)
+      return file.version.toString()
+    }
+
+    getScriptSnapshot(fileName: string) {
+      const file = this.files[fileName]
+      if (!file) return host.getScriptSnapshot(fileName)
+      return file.file
+    }
+
+    readFile(fileName: string) {
+      const file = this.files[fileName]
+      return file
+        ? file.file.getText(0, file.file.getLength())
+        : host.readFile(fileName)
+    }
+
+    fileExists(fileName: string) {
+      return this.files[fileName] !== undefined || host.fileExists(fileName)
+    }
+
+    getRouteScriptSnapshotIfUpToDate(fileName: string) {
+      const scriptVersion = this.getScriptVersion(fileName)
+      if (
+        !this.files[fileName] ||
+        scriptVersion !== host.getScriptVersion(fileName) ||
+        scriptVersion === FORCE_UPDATE_VERSION
+      ) {
+        return undefined
+      }
+      return this.files[fileName]
+    }
+    upsertRouteFile(fileName: string) {
+      const sourceFile = info.languageService
+        .getProgram()
+        ?.getSourceFile(fileName)
+      if (!sourceFile) return
+
+      const { text } = sourceFile
+      const snap = ts.ScriptSnapshot.fromString(text)
+      snap.getChangeRange = (_) => undefined
+
+      this.files[fileName] = {
+        version:
+          this.files[fileName] === undefined
+            ? FORCE_UPDATE_VERSION
+            : host.getScriptVersion(fileName),
+        file: snap,
+      }
+      return this.files[fileName]
+    }
+  }
+
+  const languageServiceHost = new VirtualLanguageServiceHost()
+  const languageService = ts.createLanguageService(languageServiceHost)
+  CACHE.set(info, { languageService, languageServiceHost })
+  return {
+    languageService,
+    languageServiceHost,
+  }
+}
+
+function getVirtualLS(
+  virtual: {
+    languageService: ts.LanguageService
+    languageServiceHost: VirtualLanguageServiceHost
+  },
+  fileName: string,
+) {
+  const result =
+    virtual.languageServiceHost.getRouteScriptSnapshotIfUpToDate(fileName) ??
+    virtual.languageServiceHost.upsertRouteFile(fileName)
+
+  if (!result) return
+
+  return {
+    toVirtualPos: (pos: number) => pos, // TODO
+    toOriginalPos: (pos: number) => pos, // TODO
+  }
+}
+
+// definitions
+// ----------------------------------------------------------------------------
+
+function getRouteDefinitions(
+  ts: TS,
+  info: ts.server.PluginCreateInfo,
+  fileName: string,
+  position: number,
+) {
+  const virtual = getVirtualLanguageService(info, ts)
+  const result = getVirtualLS(virtual, fileName)
+  if (!result) return
+  const { toOriginalPos, toVirtualPos } = result
+  const virtualPos = toVirtualPos(position)
+  const definitions = virtual.languageService.getDefinitionAndBoundSpan(
+    fileName,
+    virtualPos,
+  )
+  if (!definitions) return
+  // Assumption: This is only called when the original definitions didn't turn up anything.
+  // Therefore we are called on things like export function load ({ fetch }) .
+  // This means the textSpan needs conversion but none of the definitions because they are all referencing other files.
+  return {
+    ...definitions,
+    textSpan: {
+      ...definitions.textSpan,
+      start: toOriginalPos(definitions.textSpan.start),
+    },
+  }
+}

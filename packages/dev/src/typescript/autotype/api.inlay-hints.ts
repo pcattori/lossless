3cf9892b9f4442164bac508f1d1674272e768468
Pedro Cattori <pcattori@gmail.com>
2024-08-09 19:54:03 -0400

    refactor: autotype inlay-hints api

---
+++ packages/dev/src/typescript/autotype/api.inlay-hints.ts
@@ -0,0 +1,30 @@
+import type ts from "typescript/lib/tsserverlibrary"
+
+import { getAutotypeLanguageService } from "../autotype"
+import { type Context } from "../context"
+
+export const provideInlayHints =
+  (ctx: Context): ts.LanguageService["provideInlayHints"] =>
+  (fileName, span, preferences) => {
+    const autotype = getAutotypeLanguageService(ctx)
+    if (!autotype) return []
+
+    const route = autotype.getRoute(fileName)
+    if (!route) return []
+
+    const start = route.autotyped.toSplicedIndex(span.start)
+    return autotype
+      .provideInlayHints(
+        fileName,
+        {
+          start,
+          length:
+            route.autotyped.toSplicedIndex(span.start + span.length) - start,
+        },
+        preferences,
+      )
+      .map((hint) => ({
+        ...hint,
+        position: route.autotyped.toOriginalIndex(hint.position).index,
+      }))
+  }
--- packages/dev/src/typescript/language-service/inlay-hints.ts
+++ packages/dev/src/typescript/language-service/inlay-hints.ts
@@ -1,32 +1,12 @@
-import { getAutotypeLanguageService } from "../autotype"
+import * as Autotype from "../autotype/api.inlay-hints"
 import { type Context } from "../context"
 
 export function decorateInlayHints(ctx: Context): void {
   const ls = ctx.languageService
   const { provideInlayHints } = ls
-  ls.provideInlayHints = (fileName, span, preferences) => {
-    const fallback = () => provideInlayHints(fileName, span, preferences)
-
-    const autotype = getAutotypeLanguageService(ctx)
-    if (!autotype) return fallback()
-
-    const route = autotype.getRoute(fileName)
-    if (!route) return fallback()
-
-    const start = route.autotyped.toSplicedIndex(span.start)
-    return autotype
-      .provideInlayHints(
-        fileName,
-        {
-          start,
-          length:
-            route.autotyped.toSplicedIndex(span.start + span.length) - start,
-        },
-        preferences,
-      )
-      .map((hint) => ({
-        ...hint,
-        position: route.autotyped.toOriginalIndex(hint.position).index,
-      }))
+  ls.provideInlayHints = (...args) => {
+    return (
+      Autotype.provideInlayHints(ctx)(...args) ?? provideInlayHints(...args)
+    )
   }
 }

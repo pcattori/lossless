5604c62ace120b65ee05ab29c29540a7600b00f6
Pedro Cattori <pcattori@gmail.com>
2024-08-09 17:22:45 -0400

    refactor: autotype definitions api

--- packages/dev/src/typescript/language-service/definitions.ts
+++ packages/dev/src/typescript/language-service/definitions.ts
@@ -1,68 +1,22 @@
-import type ts from "typescript/lib/tsserverlibrary"
-
-import { getAutotypeLanguageService } from "../autotype"
+import * as Autotype from "../autotype/api.definitions"
 import { type Context } from "../context"
 
 export function decorateGetDefinition(ctx: Context) {
   const ls = ctx.languageService
+
   const { getDefinitionAndBoundSpan } = ls
-  ls.getDefinitionAndBoundSpan = (fileName, index) => {
-    const fallback = () => getDefinitionAndBoundSpan(fileName, index)
-
-    const autotype = getAutotypeLanguageService(ctx)
-    if (!autotype) return fallback()
-
-    const route = autotype.getRoute(fileName)
-    if (!route) return fallback()
-
-    const splicedIndex = route.autotyped.toSplicedIndex(index)
-
-    const result = autotype.getDefinitionAndBoundSpan(fileName, splicedIndex)
-    if (!result) return
-
-    return {
-      definitions: result.definitions?.map(toOriginalIndex(autotype)),
-      textSpan: {
-        ...result.textSpan,
-        start: route.autotyped.toOriginalIndex(result.textSpan.start).index,
-      },
-    }
+  ls.getDefinitionAndBoundSpan = (...args) => {
+    return (
+      Autotype.getDefinitionAndBoundSpan(ctx)(...args) ??
+      getDefinitionAndBoundSpan(...args)
+    )
   }
 
-  let { getTypeDefinitionAtPosition } = ls
-  ls.getTypeDefinitionAtPosition = (fileName, index) => {
-    const fallback = () => getTypeDefinitionAtPosition(fileName, index)
-
-    const autotype = getAutotypeLanguageService(ctx)
-    if (!autotype) return fallback()
-
-    const route = autotype.getRoute(fileName)
-    if (!route) return fallback()
-
-    const splicedIndex = route.autotyped.toSplicedIndex(index)
-
-    const definitions = autotype.getTypeDefinitionAtPosition(
-      fileName,
-      splicedIndex,
+  const { getTypeDefinitionAtPosition } = ls
+  ls.getTypeDefinitionAtPosition = (...args) => {
+    return (
+      Autotype.getTypeDefinitionAtPosition(ctx)(...args) ??
+      getTypeDefinitionAtPosition(...args)
     )
-    if (!definitions) return
-
-    return definitions.map(toOriginalIndex(autotype))
   }
 }
-
-const toOriginalIndex =
-  (autotype: ReturnType<typeof getAutotypeLanguageService>) =>
-  (definition: ts.DefinitionInfo): ts.DefinitionInfo => {
-    const definitionRoute = autotype.getRoute(definition.fileName)
-    if (!definitionRoute) return definition
-    return {
-      ...definition,
-      textSpan: {
-        ...definition.textSpan,
-        start: definitionRoute.autotyped.toOriginalIndex(
-          definition.textSpan.start,
-        ).index,
-      },
-    }
-  }

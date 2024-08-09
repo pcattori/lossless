dfe6c028747e18838caa1b005e1698b09c73f908
Pedro Cattori <pcattori@gmail.com>
2024-08-09 19:44:22 -0400

    refactor: autotype diagnostics api

---
+++ packages/dev/src/typescript/autotype/api.diagnostics.ts
@@ -0,0 +1,77 @@
+import type ts from "typescript/lib/tsserverlibrary"
+
+import { getAutotypeLanguageService } from "../autotype"
+import { type Context } from "../context"
+
+export const getSyntacticDiagnostics =
+  (ctx: Context): ts.LanguageService["getSyntacticDiagnostics"] =>
+  (fileName: string) => {
+    const autotype = getAutotypeLanguageService(ctx)
+    if (!autotype) return []
+    const route = autotype.getRoute(fileName)
+    if (!route) return []
+
+    const sourceFile = ctx.languageService.getProgram()?.getSourceFile(fileName)
+    if (!sourceFile) return []
+
+    return autotype
+      .getSyntacticDiagnostics(fileName)
+      .map(remapSpans(sourceFile, route.autotyped.toOriginalIndex))
+  }
+
+export const getSemanticDiagnostics =
+  (ctx: Context): ts.LanguageService["getSemanticDiagnostics"] =>
+  (fileName) => {
+    const autotype = getAutotypeLanguageService(ctx)
+    if (!autotype) return []
+    const route = autotype.getRoute(fileName)
+    if (!route) return []
+
+    const sourceFile = ctx.languageService.getProgram()?.getSourceFile(fileName)
+    if (!sourceFile) return []
+
+    return autotype
+      .getSemanticDiagnostics(fileName)
+      .map(remapSpans(sourceFile, route.autotyped.toOriginalIndex))
+  }
+
+export const getSuggestionDiagnostics =
+  (ctx: Context): ts.LanguageService["getSuggestionDiagnostics"] =>
+  (fileName) => {
+    const autotype = getAutotypeLanguageService(ctx)
+    if (!autotype) return []
+    const route = autotype.getRoute(fileName)
+    if (!route) return []
+
+    const sourceFile = ctx.languageService.getProgram()?.getSourceFile(fileName)
+    if (!sourceFile) return []
+
+    return autotype
+      .getSuggestionDiagnostics(fileName)
+      .map(remapSpans(sourceFile, route.autotyped.toOriginalIndex))
+  }
+
+const remapSpans =
+  <T extends ts.Diagnostic | ts.DiagnosticWithLocation>(
+    sourceFile: ts.SourceFile,
+    toOriginalIndex: (splicedIndex: number) => {
+      index: number
+      spliced: boolean
+    },
+  ) =>
+  (diagnostic: T): T => {
+    if (!diagnostic.start) return diagnostic
+
+    const { index, spliced } = toOriginalIndex(diagnostic.start)
+    let length = diagnostic.length
+    if (spliced) {
+      // avoid diagnostics in splices from overflowing onto unrelated code
+      length = 1
+    }
+    return {
+      ...diagnostic,
+      start: index,
+      length,
+      file: sourceFile,
+    }
+  }
--- packages/dev/src/typescript/language-service/diagnostics.ts
+++ packages/dev/src/typescript/language-service/diagnostics.ts
@@ -1,71 +1,30 @@
-import type ts from "typescript/lib/tsserverlibrary"
-
-import { getAutotypeLanguageService } from "../autotype"
+import * as Autotype from "../autotype/api.diagnostics"
 import { type Context } from "../context"
 
 export function decorateDiagnostics(ctx: Context) {
   const ls = ctx.languageService
+
   const { getSyntacticDiagnostics } = ls
-  ls.getSyntacticDiagnostics = (fileName: string) => {
+  ls.getSyntacticDiagnostics = (...args) => {
     return (
-      getRouteDiagnostics(ctx, "getSyntacticDiagnostics", fileName) ??
-      getSyntacticDiagnostics(fileName)
+      Autotype.getSyntacticDiagnostics(ctx)(...args) ??
+      getSyntacticDiagnostics(...args)
     )
   }
 
   const { getSemanticDiagnostics } = ls
-  ls.getSemanticDiagnostics = (fileName: string) => {
+  ls.getSemanticDiagnostics = (...args) => {
     return (
-      getRouteDiagnostics(ctx, "getSemanticDiagnostics", fileName) ??
-      getSemanticDiagnostics(fileName)
+      Autotype.getSemanticDiagnostics(ctx)(...args) ??
+      getSemanticDiagnostics(...args)
     )
   }
 
   const { getSuggestionDiagnostics } = ls
-  ls.getSuggestionDiagnostics = (fileName: string) => {
+  ls.getSuggestionDiagnostics = (...args) => {
     return (
-      getRouteDiagnostics(ctx, "getSuggestionDiagnostics", fileName) ??
-      getSuggestionDiagnostics(fileName)
+      Autotype.getSuggestionDiagnostics(ctx)(...args) ??
+      getSuggestionDiagnostics(...args)
     )
   }
-}
-
-function getRouteDiagnostics<
-  T extends
-    | "getSemanticDiagnostics"
-    | "getSuggestionDiagnostics"
-    | "getSyntacticDiagnostics",
->(
-  ctx: Context,
-  methodName: T,
-  fileName: string,
-): ReturnType<ts.LanguageService[T]> | undefined {
-  const autotype = getAutotypeLanguageService(ctx)
-  if (!autotype) return
-
-  const route = autotype.getRoute(fileName)
-  if (!route) return
-
-  const diagnostics: ts.Diagnostic[] = []
-  for (let diagnostic of autotype[methodName](fileName)) {
-    if (!diagnostic.start) {
-      diagnostics.push(diagnostic)
-      continue
-    }
-
-    const { index, spliced } = route.autotyped.toOriginalIndex(diagnostic.start)
-    let length = diagnostic.length
-    if (spliced) {
-      // avoid diagnostics in splices from overflowing onto unrelated code
-      length = 1
-    }
-    diagnostics.push({
-      ...diagnostic,
-      start: index,
-      length,
-      file: ctx.languageService.getProgram()?.getSourceFile(fileName),
-    })
-  }
-  // @ts-expect-error
-  return diagnostics
 }
